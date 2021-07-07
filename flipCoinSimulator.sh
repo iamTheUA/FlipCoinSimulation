63649bf782e370d10ba41ef8594f3ca087731ab5
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-07-07 21:39:27 +0530

    Number of times Head and Tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,28 @@
 #using RANDOM%2 to get 0 or 1.
-flip=RANDOM%2
+function Flip(){
+flip=$((RANDOM%2))
+}
 
 #if flip is 0, it is Head. and flip is 1, it is Tail.
+Head=0
+Tail=0
+function HeadOrTail()
+{
 if [ $flip -eq 0 ]
 then
-	echo "Head"
+	Head=$((Head+1))
 else
-	echo "Tails"
+	Tail=$((Tail+1))
 fi
+}
+
+#Running loop for 5 times
+for ((i=0;i<5;i++))
+do
+	Flip
+	HeadOrTail
+done
+
+echo "Heads: " $Head
+echo "Tails: " $Tail
 
